d6b26b5011bed9651f68fef26c9395ac431f63d0
aaronkonken <131622760+aaronkonken@users.noreply.github.com>
2023-10-27 10:09:47 +0200

    Add files via upload

---
+++ homework1/cat.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[26]:
+
+
+class cat:
+    
+    def __init__(self, name):
+        self.name = name
+        
+    def greeting(self, other_cat):
+        print("Hello my name is " + self.name + ". " + "I have plans for the world domination! Let us fight against the stupid humans. Are you with me " + other_cat.name + "?")
+
+
+# In[ ]:
+
+
+
+
